3fd37b52e60f46229ce5e2535c3e0ae1beac98b3
12Me21 <12Me21.mc@gmail.com>
2021-12-12 14:48:36 -0500

    life?

--- test.c
+++ test.c
@@ -4,8 +4,26 @@ typedef struct Pixel {
 	uint8_t r,g,b,a;
 } Pixel;
 
-void fill(Pixel* array, int length) {
-	for (int i=0; i<length; i++) {
-		array[i] = (Pixel){i,i,i,255};
+void life(int width, int height, Pixel img[height][width]) {
+	Pixel temp[height][width];
+	for (int y=0; y<height; y++) {
+		for (int x=0; x<width; x++) {
+			int neighbors = 0;
+			for (int j=-1; j<1; j++) {
+				for (int i=-1; i<1; i++) {
+					neighbors += img[(y+j+height)%height][(x+i+width)%width].r != 0;
+				}
+			}
+			int alive = img[y][x].r != 0;
+			if (neighbors==3 || (alive && neighbors==2))
+				temp[y][x] = (Pixel){255,255,255,255};
+			else
+				temp[y][x] = (Pixel){0,0,0,255};			
+		}
+	}
+	for (int y=0; y<height; y++) {
+		for (int x=0; x<width; x++) {
+			img[y][x] = temp[y][x];
+		}
 	}
 }
--- test.js
+++ test.js
@@ -8,12 +8,23 @@ var importObject = {
 	}
 }
 
-WebAssembly.instantiate(WASM_CODE, importObject).then(result => {
-	console.log(result.instance.exports)
+WebAssembly.instantiate(WASM_CODE).then(result => {
 	let b = result.instance.exports.memory.buffer
 	let data = new Uint8ClampedArray(b, 1024, 100*100*4)
 	let image = new ImageData(data, 100, 100)
 	let ctx = $canvas.getContext('2d')
-	result.instance.exports.fill(1024, 100*100)
-	ctx.putImageData(image, 0, 0)
+	for (var y=0; y<100; y++) {
+		for (var x=0; x<100; x++) {
+			if (Math.random()>0.5) {
+				data[(y*100+x)*4] = 255;
+				data[(y*100+x)*4+1] = 255;
+				data[(y*100+x)*4+2] = 255;
+			}
+			data[(y*100+x)*4+3] = 255;
+		}
+	}
+	$canvas.onclick = function() {
+		result.instance.exports.life(100, 100, 1024)
+		ctx.putImageData(image, 0, 0)
+	}
 })
